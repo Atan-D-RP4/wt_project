b56c91bbc7faba28906871a200ad2d56536c71cf
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2025-04-12 06:46:59 +0530

    Finished express/report and fixed that project

--- express/controllers/authController.ts
+++ express/controllers/authController.ts
@@ -6,7 +6,6 @@ import { Request, Response } from "express";
 import { UserModel } from "../models/user.ts";
 import { AccountModel } from "../models/account.ts";
 import * as bcrypt from "bcryptjs";
-import { Request } from "../../../../../.cache/deno/npm/registry.npmjs.org/undici-types/6.20.0/fetch.d.ts";
 
 export const authController = {
   register: async (req: Request, res: Response) => {
